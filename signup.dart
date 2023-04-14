611aa5139fd0e96b8a70a2f2206afd8407bc57c2
Alaakhaled1911 <125863248+Alaakhaled1911@users.noreply.github.com>
2023-04-14 23:06:12 +0200

    Update and rename signup.md to signup.dart

---
+++ signup.dart
@@ -0,0 +1,123 @@
+
+ 
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  TextEditingController usernameController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  TextEditingController emailController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+          gradient: LinearGradient(
+        begin: Alignment.topRight,
+        end: Alignment.bottomLeft,
+        colors: [
+          Color.fromARGB(255, 182, 195, 204),
+          Color.fromARGB(255, 230, 159, 202),
+        ],
+      )),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: _page(),
+      ),
+    );
+  }
+
+  Widget _page() {
+    return Padding(
+      padding: const EdgeInsets.all(32.0),
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            _icon(),
+            const SizedBox(height: 50),
+            _inputField("Username", usernameController),
+            const SizedBox(height: 20),
+            _inputField("Password", passwordController, isPassword: true),
+            const SizedBox(height: 20),
+            _inputField("Email", emailController),
+            const SizedBox(height: 20),
+            _loginBtn(),
+            const SizedBox(height: 20),
+            _extraText(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _icon() {
+    return Container(
+      decoration: BoxDecoration(
+          border: Border.all(color: Colors.white, width: 2),
+          shape: BoxShape.circle),
+      child: const Icon(Icons.person, color: Colors.white, size: 120),
+    );
+  }
+
+  Widget _inputField(String hintText, TextEditingController controller,
+      {isPassword = false}) {
+    var border = OutlineInputBorder(
+        borderRadius: BorderRadius.circular(18),
+        borderSide: const BorderSide(color: Colors.white));
+
+    return TextField(
+      style:  TextStyle(color: Colors.white),
+      controller: controller,
+      decoration: InputDecoration(
+        hintText: hintText,
+        hintStyle:  TextStyle(color: Colors.white),
+        enabledBorder: border,
+        focusedBorder: border,
+      ),
+      obscureText: isPassword,
+    );
+  }
+
+  Widget _loginBtn() {
+    return ElevatedButton(
+      onPressed: () {
+        debugPrint("Username : " + usernameController.text);
+        debugPrint("Password : " + passwordController.text);
+           debugPrint("email : " + emailController.text);
+      },
+      child: const SizedBox(
+          width: double.infinity,
+          child: Text(
+            "Sign in ",
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 20),
+          )),
+      style: ElevatedButton.styleFrom(
+        shape: const StadiumBorder(),
+        primary: Colors.white,
+        onPrimary: Color.fromARGB(255, 141, 141, 141),
+        padding: const EdgeInsets.symmetric(vertical: 16),
+      ),
+    );
+  }
+
+  Widget _extraText() {
+    return const Text(
+      "Can't create account?",
+      textAlign: TextAlign.center,
+      style: TextStyle(fontSize: 16, color: Colors.white),
+    );
+  }
+}
+
+
+
+
+
+
+
+
